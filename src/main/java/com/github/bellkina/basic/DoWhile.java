b02bcb4db823fefbc19a0e540b1bfa4bd3713213
Bellkin <padlabearel@gmail.com>
2017-07-09 16:30:10 +0400

    feat: add maven framework support

---
+++ src/main/java/com/github/bellkina/basic/DoWhile.java
@@ -0,0 +1,10 @@
+package com.github.bellkina.basic;
+
+public class DoWhile {
+    public static void main(String args[]) {
+        int n = 10;
+        do {
+            System.out.println("tick " + n);
+        } while (--n > 0);
+    }
+}
