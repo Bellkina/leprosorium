b7f90c3115bb6b83484a9ab4459e50604e1a686d
padlaboris <padlabearel@gmail.com>
2017-07-13 20:07:59 +0400

    feat: fix issues: - rename package - create new packages - add new examples

---
+++ src/main/java/com/github/padlaboris/Main.java
@@ -0,0 +1,8 @@
+package com.github.padlaboris;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/com/github/padlaboris/basic/DoWhile.java
@@ -0,0 +1,10 @@
+package com.github.padlaboris.basic;
+
+public class DoWhile {
+    public static void main(String args[]) {
+        int n = 10;
+        do {
+            System.out.println("tick " + n);
+        } while (--n > 0);
+    }
+}
