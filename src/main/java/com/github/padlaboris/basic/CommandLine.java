97026584f9fd36bb9a8cc5d763cd044ddf3ddbb4
padlaboris <padlabearel@gmail.com>
2017-07-25 15:26:19 +0400

    feat: add new package with new examples

---
+++ src/main/java/com/github/padlaboris/basic/CommandLine.java
@@ -0,0 +1,19 @@
+package com.github.padlaboris.basic;
+
+public class CommandLine {
+    public static void main(String args[]) {
+        for (int i = 0; i < 1; i++)
+            System.out.println("args[" + 0 + "]: " + " this ");
+        for (int i = 0; i < 1; i++)
+            System.out.println("args[" + 1 + "]: " + " is ");
+        for (int i = 0; i < 1; i++)
+            System.out.println("args[" + 2 + "]: " + " a ");
+        for (int i = 0; i < 1; i++)
+            System.out.println("args[" + 3 + "]: " + " test ");
+        for (int i = 0; i < 1; i++)
+            System.out.println("args[" + 4 + "]: " + " 100 ");
+        for (int i = 0; i < 1; i++)
+            System.out.println("args[" + 5 + "]: " + " -1 ");
+
+    }
+}
