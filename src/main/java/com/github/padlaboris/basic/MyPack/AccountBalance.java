9a71c35790f64352cf375d7d5f3286b0b88d5061
padlaboris <padlabearel@gmail.com>
2017-08-27 16:14:24 +0400

    feat: fix issues - create new packages - add new examples - reformat file Balance

--- src/main/java/com/github/padlaboris/basic/MyPack/AccountBalance.java
+++ src/main/java/com/github/padlaboris/basic/MyPack/AccountBalance.java
@@ -1,21 +1,5 @@
 package com.github.padlaboris.basic.MyPack;
 
-class Balance {
-    String name;
-    double bal;
-
-    Balance(String n, double b) {
-        name = n;
-        bal = b;
-    }
-
-    void show() {
-        if (bal < 0)
-            System.out.print("==> ");
-        System.out.println(name + ": $" + bal);
-    }
-}
-
 class AccountBalance {
     public static void main(String args[]) {
         Balance current[] = new Balance[3];
---
+++ src/main/java/com/github/padlaboris/basic/Packages/p2/Protection2.java
@@ -0,0 +1,36 @@
+package com.github.padlaboris.basic.Packages.p2;
+
+import com.github.padlaboris.basic.Packages.p1.Protection;
+
+class Protection2 extends Protection {
+    Protection2() {
+        System.out.println("derived other package constructor");
+
+        // class or package only
+        // System.out.println("n = " + n);
+
+        // class only
+        // System.out.println("n_pri = " + n_pri);
+
+        System.out.println("n_pro = " + n_pro);
+        System.out.println("n_pub = " + n_pub);
+    }
+}
+
+class OtherPackage {
+    OtherPackage() {
+        Protection p = new Protection();
+        System.out.println("other package constructor");
+
+        // class or package only
+        // System.out.println("n = " + n);
+
+        // class only
+        // System.out.println("n_pri = " + p.n_pri);
+
+        // class, subclass or package only
+        // System.out.println("n_pro = " + p.n_pro);
+
+        System.out.println("n_pub = " + p.n_pub);
+    }
+}
